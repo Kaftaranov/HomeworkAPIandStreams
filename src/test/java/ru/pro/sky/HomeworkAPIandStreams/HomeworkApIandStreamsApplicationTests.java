65472f16ef29e4249816cbe7bfa1dc7670b6e28c
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-08-01 17:38:08 +0600

    Initial commit

---
+++ src/test/java/ru/pro/sky/HomeworkAPIandStreams/HomeworkApIandStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.HomeworkAPIandStreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApIandStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
