5ab30be4a6d4aae0787451a27a88076e82cfa60b
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-08-01 17:38:08 +0600

    Initial commit

---
+++ src/main/java/ru/pro/sky/HomeworkAPIandStreams/HomeworkApIandStreamsApplication.java
@@ -0,0 +1,13 @@
+package ru.pro.sky.HomeworkAPIandStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkApIandStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkApIandStreamsApplication.class, args);
+	}
+
+}
